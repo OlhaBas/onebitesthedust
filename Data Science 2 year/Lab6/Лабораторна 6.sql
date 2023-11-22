7249c5903923837b0e6a48d369399c2cae166da5
OlhaBas <144379158+OlhaBas@users.noreply.github.com>
2023-11-22 18:29:03 +0100

    Update Лабораторна 6.sql

--- Data Science 2 year/Lab6/Лабораторна 6.sql
+++ Data Science 2 year/Lab6/Лабораторна 6.sql
@@ -1,34 +1,54 @@
-CREATE TABLE categories 
-(
-    category_id INT PRIMARY KEY,
-    category_name VARCHAR(255)
+1 Запит
+SELECT c.name as city_name, r.name as region_name, r.area_quantity
+FROM cities c
+JOIN regions r ON c.region = r.uuid
+WHERE c.population > 350000;
+
+2 Запит
+SELECT c.name AS city_name, r.name AS region_name
+FROM cities c
+JOIN regions r ON c.region = r.uuid
+WHERE r.name = 'Nord';
+
+CREATE TABLE stations 
+    (
+    station INT PRIMARY KEY,
+    name VARCHAR(255),
+    latitude DECIMAL(10, 8),
+    longitude DECIMAL(11, 8)
 );
 
-CREATE TABLE news
- (
-    news_id INT PRIMARY KEY,
-    category_id INT,
-    title_name VARCHAR(255),
-    content _text TEXT,
-    
-    FOREIGN KEY (category_id) REFERENCES Categories(category_id)
+CREATE TABLE metro_lines 
+    (
+    line INT PRIMARY KEY,
+    name VARCHAR(255)
 );
 
-CREATE TABLE comments
- (
-    comment_id INT PRIMARY KEY,
-    news_id INT,
-    comment_text TEXT,
-    comment_date TIMESTAMP,
-    FOREIGN KEY (news_id) REFERENCES News(news_id)
+CREATE TABLE stations_on_line 
+    (
+    station INT,
+    line INT,
+    sequence_number INT,
+    PRIMARY KEY (station, line),
+    FOREIGN KEY (station) REFERENCES stations(station),
+    FOREIGN KEY (line) REFERENCES metro_lines(line)
 );
 
-CREATE TABLE ratings
- (
-    rating_id INT PRIMARY KEY,
-    news_id INT,
-    ip_address VARCHAR(45),
-    rating_value INT CHECK (rating_value >= 1 AND rating_value <= 5),
-    UNIQUE(news_id, ip_address), -- Унікальність оцінки для кожної новини від одного IP
-    FOREIGN KEY (news_id) REFERENCES News(news_id)
+CREATE TABLE station_connections 
+    (
+    connection INT PRIMARY KEY,
+    station_1 INT,
+    station_2 INT,
+    FOREIGN KEY (station_1) REFERENCES stations(station),
+    FOREIGN KEY (station_2) REFERENCES stations(station)
+);
+
+CREATE TABLE line_description 
+    (
+    line INT PRIMARY KEY,
+    color VARCHAR(50),
+    length DECIMAL(10, 2),
+    travel_time TIME,
+    description TEXT,
+    FOREIGN KEY (line) REFERENCES metro_lines(line)
 );
