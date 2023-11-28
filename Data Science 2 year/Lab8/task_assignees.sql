babe4a2f63f928c79fcf206567b40294e97aacb1
OlhaBas <144379158+OlhaBas@users.noreply.github.com>
2023-11-28 08:16:58 +0100

    Update task_assignees.sql

--- Data Science 2 year/Lab8/task_assignees.sql
+++ Data Science 2 year/Lab8/task_assignees.sql
@@ -1,3 +1,17 @@
+SET NAMES utf8;
+SET time_zone = `+00:00`;
+SET foreign_key_checks = 0;
+SET sql_mode = `NO_AUTO_VALUE_ON_ZERO`;
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS projects;
+DROP TABLE IF EXISTS users_projects;
+DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS user_tasks;
+DROP TABLE IF EXISTS task_assignees;
+DROP TABLE IF EXISTS files;
+
+
 CREATE TABLE users 
   (
     User INT PRIMARY KEY AUTO_INCREMENT,
@@ -6,17 +20,17 @@ CREATE TABLE users
     Password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS users_projects
-  (
-   Project_id INT(256) UNSIGNED NOT NULL,
-   User_id INT(256) UNSIGNED NOT NULL  
-  );
-
 CREATE TABLE projects 
   (
     Project INT PRIMARY KEY AUTO_INCREMENT,
     ProjectName VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE users_projects
+  (
+   Project_id INT(256) UNSIGNED NOT NULL,
+   User_id INT(256) UNSIGNED NOT NULL  
+  );
 
 CREATE TABLE tasks 
   (
@@ -28,6 +42,15 @@ CREATE TABLE tasks
     FOREIGN KEY (Author) REFERENCES Users(User),
     FOREIGN KEY (Project) REFERENCES Projects(Project)
 );
+
+CREATE TABLE user_tasks 
+  (
+   user_id INT,
+   task_id INT,
+   PRIMARY KEY (user_id, task_id),
+   FOREIGN KEY (user_id) REFERENCES  users(user),
+   FOREIGN KEY (task_id) REFERENCES  tasks(task)
+  );
 
 CREATE TABLE task_assignees 
   (
