0777fd4c23313cddb5db50b82fdfd730862335b0
OlhaBas <144379158+OlhaBas@users.noreply.github.com>
2023-11-28 08:26:48 +0100

    Update task_assignees.sql

--- Data Science 2 year/Lab8/task_assignees.sql
+++ Data Science 2 year/Lab8/task_assignees.sql
@@ -14,33 +14,33 @@ DROP TABLE IF EXISTS files;
 
 CREATE TABLE users 
   (
-    User INT PRIMARY KEY AUTO_INCREMENT,
-    Username VARCHAR(255) NOT NULL,
-    Email VARCHAR(255) NOT NULL,
-    Password VARCHAR(255) NOT NULL
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE projects 
   (
-    Project INT PRIMARY KEY AUTO_INCREMENT,
-    ProjectName VARCHAR(255) NOT NULL
+    project INT PRIMARY KEY AUTO_INCREMENT,
+    project_name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE users_projects
   (
-   Project_id INT(256) UNSIGNED NOT NULL,
-   User_id INT(256) UNSIGNED NOT NULL  
+   project_id INT(256) UNSIGNED NOT NULL,
+   user_id INT(256) UNSIGNED NOT NULL  
   );
 
 CREATE TABLE tasks 
   (
-    Task INT PRIMARY KEY AUTO_INCREMENT,
-    TaskName VARCHAR(255) NOT NULL,
-    Description TEXT,
-    Author INT,
-    Project INT,
-    FOREIGN KEY (Author) REFERENCES Users(User),
-    FOREIGN KEY (Project) REFERENCES Projects(Project)
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    task_mame VARCHAR(255) NOT NULL,
+    description TEXT,
+    author INT,
+    project INT,
+    FOREIGN KEY (author) REFERENCES users(id),
+    FOREIGN KEY (project) REFERENCES projects(project)
 );
 
 CREATE TABLE user_tasks 
@@ -48,26 +48,17 @@ CREATE TABLE user_tasks
    user_id INT,
    task_id INT,
    PRIMARY KEY (user_id, task_id),
-   FOREIGN KEY (user_id) REFERENCES  users(user),
-   FOREIGN KEY (task_id) REFERENCES  tasks(task)
+   FOREIGN KEY (user_id) REFERENCES  users(id),
+   FOREIGN KEY (task_id) REFERENCES  tasks(id)
   );
-
-CREATE TABLE task_assignees 
-  (
-    TaskAssignee INT PRIMARY KEY AUTO_INCREMENT,
-    Task INT,
-    User INT,
-    FOREIGN KEY (Task) REFERENCES Tasks(Task),
-    FOREIGN KEY (User) REFERENCES Users(User)
-);
 
 CREATE TABLE files 
   (
-    File INT PRIMARY KEY AUTO_INCREMENT,
-    FileName VARCHAR(255) NOT NULL,
-    FilePath VARCHAR(255) NOT NULL,
-    Task INT,
-    Project INT,
-    FOREIGN KEY (Task) REFERENCES Tasks(Task),
-    FOREIGN KEY (Project) REFERENCES Projects(Project)
+    file INT PRIMARY KEY AUTO_INCREMENT,
+    file_name VARCHAR(255) NOT NULL,
+    file_path VARCHAR(255) NOT NULL,
+    task INT,
+    project INT,
+    FOREIGN KEY (task) REFERENCES tasks(task),
+    FOREIGN KEY (project) REFERENCES projects(project)
 );
